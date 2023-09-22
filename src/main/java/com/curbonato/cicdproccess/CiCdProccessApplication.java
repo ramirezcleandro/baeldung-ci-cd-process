f09249a48f169db69b75645aa0ba5ed5f72b9e30
Leandro <lramirez@miranchosrl.com>
2023-09-22 10:42:33 -0400

    First commit

---
+++ src/main/java/com/curbonato/cicdproccess/CiCdProccessApplication.java
@@ -0,0 +1,13 @@
+package com.curbonato.cicdproccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdProccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdProccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curbonato/cicdproccess/CiCdProccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curbonato.cicdproccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdProccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
